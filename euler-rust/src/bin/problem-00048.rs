4c9319b9a8323a178fe0775e2fbc513cbf6dc895
nuke <abhishekbanerjee.iitd@gmail.com>
2024-12-21 23:04:45 -0800

    Problem 48 solution in Rust.

---
+++ euler-rust/src/bin/problem-00048.rs
@@ -0,0 +1,15 @@
+use num_bigint::BigUint;
+use num_traits::cast::ToPrimitive;
+
+fn main() {
+    let digits = self_power_last_ten(1000);
+    println!("{}", digits);
+}
+
+fn self_power_last_ten(limit: u32) -> u64 {
+    // Use the big-int library for the large exponentiation. Sum up
+    // the series and extract the last 10 digits by taking the
+    // remainder from a division by 10^10.
+    ((1..=limit).map(|n| BigUint::from(n).pow(n)).sum::<BigUint>()
+     % BigUint::from(10u8).pow(10)).to_u64().unwrap()
+}
