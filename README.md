063432a42a478b0243709ae49800a728900f7a84
nuke <abhishekbanerjee.iitd@gmail.com>
2025-01-01 20:16:08 -0800

    Problem 78 solution in Rust.

--- README.md
+++ README.md
@@ -92,3 +92,4 @@ $ cargo run --bin problem-00001
 | 75   | [Singular Integer Right Triangles](https://projecteuler.net/problem=75) | [problem-00075.rs](euler-rust/src/bin/problem-00075.rs) | 161667           |
 | 76   | [Counting Summations](https://projecteuler.net/problem=76)   | [problem-00076.rs](euler-rust/src/bin/problem-00076.rs) | 190569291        |
 | 77   | [Prime Summations](https://projecteuler.net/problem=77)      | [problem-00077.rs](euler-rust/src/bin/problem-00077.rs) | 71               |
+| 78   | [Coin Partitions](https://projecteuler.net/problem=78)       | [problem-00078.rs](euler-rust/src/bin/problem-00078.rs) | 55374            |
---
+++ euler-rust/src/bin/problem-00078.rs
@@ -0,0 +1,45 @@
+use num_bigint::BigInt;
+
+fn main() {
+    let num = partition_divisible_by(1_000_000);
+    println!("{}", num);
+}
+
+// We calculate partitions of numbers, using the exact same method as
+// Problem 76, until our divisibility condition is satisfied. There
+// are some changes, to grow the cache of generalized pentagonals on
+// demand, and to handle big numbers.
+fn partition_divisible_by(d: u32) -> usize {
+    let mut pentagonals = Vec::new();
+    generate_gen_pentagonal(&mut pentagonals);
+    let mut partitions: Vec<BigInt> = vec![BigInt::from(1)];
+    let mut n = 1usize;
+    loop {
+	if &n > pentagonals.last().unwrap() {
+	    generate_gen_pentagonal(&mut pentagonals);
+	}
+	let mut p = BigInt::ZERO;
+	let mut sign = BigInt::from(1);
+	for idx in (0..pentagonals.len()).step_by(2) {
+	    if pentagonals[idx] > n { break; }
+	    p += &sign * &partitions[n - pentagonals[idx]];
+	    if pentagonals[idx+1] > n { break; }
+	    p += &sign * &partitions[n - pentagonals[idx+1]];
+	    sign *= -1;
+	}
+	partitions.push(p.clone());
+	if p % BigInt::from(d) == BigInt::ZERO { break; }
+	n += 1;
+    }
+    n
+}
+
+// We generate and store generalized pentagonal numbers for easy
+// lookup later. Generate 1000 pentagonals at a time.
+fn generate_gen_pentagonal(pentagonals: &mut Vec<usize>) {
+    let l = pentagonals.len();
+    for k in (l/2+1)..=(l/2+500) {
+	pentagonals.push(k * (3 * k - 1) / 2);
+	pentagonals.push(k * (3 * k + 1) / 2);
+    }
+}
